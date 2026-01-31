0b61217bc0b987b87da76ab4a17bab98facef95b
I-am-Chittesh <iamchitteshd@gmail.com>
2026-01-31 19:00:52 +0530

    inital_commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Hero from "../components/sections/Hero";
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="bg-[#0a0a0a] min-h-screen text-white">
       <Hero />
-      {/* We will add BentoGrid and Footer next */}
+      {/* Next Step: <BentoGrid /> */}
     </main>
   );
 }
