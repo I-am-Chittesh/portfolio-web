8adfa7b7f83f7f5b3a0de32704abf817dd05eb71
I-am-Chittesh <iamchitteshd@gmail.com>
2026-01-31 18:44:00 +0530

    first_commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "../components/sections/Hero";
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <Hero />
+      {/* We will add BentoGrid and Footer next */}
+    </main>
+  );
+}
