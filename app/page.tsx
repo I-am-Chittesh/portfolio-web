241deac57e05561ad2ccc75d0f0ebc75131f92cb
I-am-Chittesh <iamchitteshd@gmail.com>
2026-02-09 21:16:24 +0530

    Flush

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import {Hero} from "@/components/sections/Hero";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-[#0a0a0a]">
-      <Hero />
-    </main>
-  );
-}
